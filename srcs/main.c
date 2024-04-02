0335f3e739f3a953aaed6691575272bc039a203b
Aether Guez <aet7gz@gmail.com>
2024-04-02 15:36:34 +0200

    add "exit" command heandling before any other operations are executed

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,47 @@
 /*   By: caguillo <caguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 22:55:50 by caguillo          #+#    #+#             */
-/*   Updated: 2024/04/02 15:07:18 by arguez           ###   ########.fr       */
+/*   Updated: 2024/04/02 15:32:13 by arguez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void	quitter(char *prompt)
+{
+	free(prompt);
+	exit(0);
+}
+
+void	read_prompt(t_mini *mini)
+{
+	char	*prompt;
+
+	prompt = readline("~$ ");
+	if (prompt)
+	{
+		add_history(prompt);
+		if (ft_strcmp(prompt, "exit") == 0)
+			quitter(prompt);
+		mini->fprompt = format_prompt(prompt);
+		free(prompt);
+		mini->token = ft_split(mini->fprompt, ' ');
+		mini->type = create_type(mini->token);
+		check_type(mini->type, mini->token);
+		// temp for checking
+		printf("%s\n", mini->fprompt);
+		temp_display_tabs(mini->token, mini->type);
+		// free here for now
+		double_free((void **)mini->token);
+		free(mini->fprompt);
+		free(mini->type);
+	}
+	else
+	{
+		ft_putstr_fd(ERR_RDL, 2);
+		mini->exitcode = EXIT_FAILURE;
+	}
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -41,39 +77,11 @@ int	main(int argc, char **argv, char **envp)
 	return (0);
 }
 
-void	read_prompt(t_mini *mini)
-{
-	char	*prompt;
-
-	prompt = readline("~$ ");
-	if (prompt)
-	{
-		add_history(prompt);
-		(*mini).fprompt = format_prompt(prompt);
-		free(prompt);
-		(*mini).token = ft_split((*mini).fprompt, ' ');
-		(*mini).type = create_type((*mini).token);
-		check_type((*mini).type, (*mini).token);
-		// temp for checking
-		printf("%s\n", (*mini).fprompt);
-		temp_display_tabs((*mini).token, (*mini).type);
-		// free here for now
-		double_free((void **)mini->token);
-		free((*mini).fprompt);
-		free((*mini).type);
-	}
-	else
-	{
-		ft_putstr_fd(ERR_RDL, 2);
-		(*mini).exitcode = EXIT_FAILURE;
-	}
-}
-
 // if (syntax_error(prompt) == 0)
 // 		{
 // 		}
 // 		else
 // 		{
 // 			ft_putstr_fd(ERR_STX, 2);
-// 			(*mini).exitcode = EXIT_STX;
+// 			mini->exitcode = EXIT_STX;
 // 		}
